15eb96360bb909b822b239259c2f95da3ac36682
kalinchkma <chakma.kalin10211@gmail.com>
2024-06-04 01:03:37 +0600

    actix-web package added and hello world server implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/hello-world")]
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+async fn echo() -> impl Responder {
+    HttpResponse::Ok().body("echo.....")
+}
+
+#[actix_web::main] 
+async fn main() -> std::io::Result<()> {
+    const ADDRESS: &str = "127.0.0.1";
+    const PORT: u16 = 6060;
+    println!("Server running on http:/{}:{}/", ADDRESS, PORT);
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+            .route("/echo", web::get().to(echo))  
+    }).bind((ADDRESS, PORT))?
+    .run()
+    .await
 }
